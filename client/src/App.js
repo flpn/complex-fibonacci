a5511e0c4d305afdfdcd85f88be4d6161f79fb3c
Felipe Netto <flpncc@gmail.com>
2022-03-16 10:24:32 -0300

    Fix some bugs

--- client/src/App.js
+++ client/src/App.js
@@ -7,9 +7,8 @@ import Fibonacci from './Fibonacci'
 function App() {
   return (
     <Router>
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
+      <div>
+        <header>
           <Link to='/'>Home</Link>
           <Link to='/otherpage'>Other Page</Link>
         </header>
